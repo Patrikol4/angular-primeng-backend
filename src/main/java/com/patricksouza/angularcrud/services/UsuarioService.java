25437660e3ffaf4a7ff7e6786ca1af612ab95689
Patrikol4 <doublecrt@gmail.com>
2021-06-20 01:01:10 -0300

    Listando todos os usuarios

--- src/main/java/com/patricksouza/angularcrud/services/UsuarioService.java
+++ src/main/java/com/patricksouza/angularcrud/services/UsuarioService.java
@@ -1,21 +1,29 @@
 package com.patricksouza.angularcrud.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.patricksouza.angularcrud.domain.Usuario;
+import com.patricksouza.angularcrud.exceptions.ObjectNotFoundException;
 import com.patricksouza.angularcrud.repositories.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	public Usuario findById(Integer id) {
 		Optional<Usuario> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id:  " + id + "Do Tipo : " + Usuario.class.getName()));
+	}
+
+	public List<Usuario> findAll() {
+
+		return repository.findAll();
 	}
 }
